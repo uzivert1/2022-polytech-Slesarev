0e654f60ac9c510d54fb2a48e8e8cb191578093e
Monetchik <slesarev.ayu@edu.spbstu.ru>
2022-12-09 14:15:42 +0300

    complex8

---
+++ 08_oop/complex/complex.h
@@ -0,0 +1,15 @@
+#ifndef COMPLEX_HPP
+#define COMPLEX_HPP
+
+class Complex {
+public:
+  int r_part;
+  int i_part;
+  Complex(int r, int i) : r_part(r), i_part(i){};
+  const Complex operator+(const Complex rv) const;
+  const Complex operator-(const Complex rv) const;
+  const bool operator==(const Complex rv) const;
+  const double abs() const;
+};
+
+#endif
